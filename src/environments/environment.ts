64b6bf30d165aa6136af044e65d7b374670c0ca0
danaboude <danaboude@gmail.com>
2025-12-23 00:43:32 +0100

    fix: restore missing environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    backendUrl: 'http://localhost:3001',
+    mqtt: {
+        server: 'ostrich.lmq.cloudamqp.com',
+        protocol: 'wss',
+        port: 443,
+        path: '/mqtt',
+        username: 'shporlek',
+        password: '1Q5wCZyc3iCOw5TLI1M-lFwjVdM86NGa',
+    }
+};
